c0a842c661686b0e36ed42d789f63ff72146f0db
manisrinivasa1999 <manisrinivas1999@gmail.com>
2022-01-13 09:41:34 +0530

    add html templates

--- challenges/views.py
+++ challenges/views.py
@@ -23,12 +23,14 @@ monthly_challenges = {
 def index(request):
     list_items = ''
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalize_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalize_month}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+
+    # for month in months:
+    #     capitalize_month = month.capitalize()
+    #     month_path = reverse('month-challenge', args=[month])
+    #     list_items += f"<li><a href='{month_path}'>{capitalize_month}</a></li>"
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {'months': months})
 
 
 def monthly_challenge_by_number(request, month):
@@ -43,7 +45,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f'<h1>{challenge_text}</h1>'
-        return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html', {
+            "text": challenge_text,
+            "month": month,
+        })
+        # response_data = render_to_string('challenges/challenge.html')
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound('<h1>This month is not supported.</h1>')
